63c5dfb76df93ea6b4edfedf554524927fa42ea9
Dmytro <dmytro@paybee.io>
2024-07-16 15:11:02 +0300

    test

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     URI: 'https://platform.fintacharts.com',
     USERNAME: 'r_test@fintatech.com',
-    production: 'undefined',
     PASSWORD:'kisfiz-vUnvy9-sopnyv',
     URI_WSS: 'wss://platform.fintacharts.com', 
 };
